976aac6aafb7399bb7cbf26c0b3498a77c468d1e
Kenny Fryar-Ludwig <fryarludwig@gmail.com>
2016-10-08 17:48:04 -0600

    Added 'getAllCourses' API call

--- app/src/main/java/com/batterystapler/staplapp/API.kt
+++ app/src/main/java/com/batterystapler/staplapp/API.kt
@@ -1,7 +1,9 @@
 package com.batterystapler.staplapp
 
+import com.batterystapler.staplapp.models.Course
 import com.lightningkite.kotlin.networking.NetEndpoint
 import com.lightningkite.kotlin.networking.NetInterface
+import com.lightningkite.kotlin.networking.gsonGet
 
 /**
  * Created by fryar on 10/8/2016.
@@ -12,4 +14,9 @@ object API : NetInterface(){
     val course_call = root.sub("/course")
     val chapter_call = root.sub("/chapter")
     val stapl_call = root.sub("/stapl")
+
+    fun GetAllCourses(callback: (Array<Course>?) -> Unit) = course_call.gsonGet<Array<Course>>(
+            onError = {callback(null); true},
+            onResult = {print(it); callback(it)}
+    )
 }
